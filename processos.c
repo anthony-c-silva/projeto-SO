28d1341cc7193abc29395286795a1dc974d104e0
Anthony <anthony.silva@avmb.com.br>
2023-10-01 21:59:54 -0300

    feat - adicionando so espera proc e verifica e desbloquea dispositivos

--- processos.c
+++ processos.c
@@ -14,6 +14,17 @@ Processo tabela_processos[MAX_PROCESSOS];
 int processo_em_execucao = -1;
 int quantum_restante = 0;
 
+int processos_esperando[MAX_PROCESSOS]; // Lista de processos esperando por outros
+int num_processos_esperando = 0; // Contador de processos esperando
+
+typedef struct {
+    int pid_bloqueado; // PID do processo bloqueado
+    int dispositivo; // Número do dispositivo
+} Dispositivo;
+
+Dispositivo dispositivos_bloqueados[MAX_PROCESSOS]; // Lista de dispositivos bloqueados
+int num_dispositivos_bloqueados = 0; // Contador de dispositivos bloqueados
+
 // Implementação de uma fila simples de processos prontos
 int fila_prontos[MAX_PROCESSOS];
 int inicio_fila = 0;
@@ -101,4 +112,41 @@ void encerrar_processo(int pid) {
 void realizar_entrada_saida(int pid) {
     // Implementar a lógica para realizar E/S em um terminal diferente com base no PID
     printf("Realizando E/S para o processo com PID %d\n", pid);
+}
+
+void verificar_e_desbloquear_dispositivos() {
+    // Implementar a lógica para verificar e desbloquear dispositivos
+    // Percorrer a lista de dispositivos bloqueados e verifique se eles estão prontos
+    // Desbloquear os processos apropriados conforme necessário
+}
+
+void so_espera_proc(int pid_esperado) {
+    // Adicionar o processo chamador à lista de processos esperando
+    processos_esperando[num_processos_esperando] = processo_em_execucao;
+    num_processos_esperando++;
+
+    // Bloquear o processo chamador e execute o escalonador
+    tabela_processos[processo_em_execucao].estado = 1; // Define o estado como bloqueado
+    escalonador();
+
+    // Verificar se o processo esperado terminou e desbloqueie o processo chamador
+    while (1) {
+        int processo_encontrado = 0;
+        for (int i = 0; i < MAX_PROCESSOS; i++) {
+            if (tabela_processos[i].pid == pid_esperado) {
+                processo_encontrado = 1;
+                break;
+            }
+        }
+        if (!processo_encontrado) {
+            break; // O processo esperado terminou, saia do loop
+        }
+    }
+
+    // Desbloquear o processo chamador e remover da lista de processos esperando
+    tabela_processos[processo_em_execucao].estado = 0; // Define o estado como pronto
+    num_processos_esperando--;
+
+    // Executa o escalonador novamente
+    escalonador();
 }
