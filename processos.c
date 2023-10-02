8ea9f4c3f7a0b5176e809d38800a32811be0dfd4
Anthony <anthony.silva@avmb.com.br>
2023-10-01 21:38:07 -0300

    feat - fazendo um implementação simples do  Escalonador Preemptivo Round-Robin

--- processos.c
+++ processos.c
@@ -2,17 +2,42 @@
 #include <stdio.h>
 
 #define MAX_PROCESSOS 100
+#define QUANTUM 4 // Valor do quantum (ajuste conforme necessário)
+
+typedef struct {
+    int pid;
+    int estado; // 0 para pronto, 1 para bloqueado, 2 para executando
+    // Outros campos para informações do processo, se necessário
+} Processo;
 
 Processo tabela_processos[MAX_PROCESSOS];
-
 int processo_em_execucao = -1;
+int quantum_restante = 0;
+
+// Implementação de uma fila simples de processos prontos
+int fila_prontos[MAX_PROCESSOS];
+int inicio_fila = 0;
+int fim_fila = 0;
+
+// Função para adicionar um processo à fila de prontos
+void adicionar_a_fila(int pid) {
+    fila_prontos[fim_fila] = pid;
+    fim_fila = (fim_fila + 1) % MAX_PROCESSOS;
+}
+
+// Função para escolher o próximo processo a ser executado
+int escolher_proximo() {
+    if (inicio_fila != fim_fila) {
+        int pid = fila_prontos[inicio_fila];
+        inicio_fila = (inicio_fila + 1) % MAX_PROCESSOS;
+        return pid;
+    }
+    return -1; // Retorna -1 se a fila estiver vazia
+}
 
 void init_processos() {
-    for (int i = 0; i < MAX_PROCESSOS; i++) {
-        tabela_processos[i].pid = -1;
-        tabela_processos[i].estado = 0;
-        // Inicialize outras informações do processo, se necessário
-    }
+    // Inicialize a tabela de processos e outras variáveis globais
+    // ...
 }
 
 void salvar_estado(Processo *processo) {
@@ -26,14 +51,18 @@ void recuperar_estado(Processo *processo) {
 }
 
 void escalonador() {
-    // Implemente um escalonador simples, por exemplo, selecionando o próximo processo pronto
-    for (int i = 0; i < MAX_PROCESSOS; i++) {
-        if (tabela_processos[i].estado == 1) {
-            processo_em_execucao = i;
-            printf("Processo com PID %d está em execução\n", tabela_processos[i].pid);
-            break;
+    if (quantum_restante == 0) {
+        if (processo_em_execucao != -1) {
+            salvar_estado(&tabela_processos[processo_em_execucao]);
+            adicionar_a_fila(processo_em_execucao);
+        }
+        processo_em_execucao = escolher_proximo();
+        quantum_restante = QUANTUM;
+        if (processo_em_execucao != -1) {
+            recuperar_estado(&tabela_processos[processo_em_execucao]);
         }
     }
+    quantum_restante--;
 }
 
 void tratador_interrupcao() {
